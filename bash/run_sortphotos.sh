6eba6d04baf12268de529141a54ae9f4017d7ba6
mathieu <mcornille@gmail.com>
2020-07-22 20:45:53 +0100

    Changing file permissions before running script

--- bash/run_sortphotos.sh
+++ bash/run_sortphotos.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+/bin/chmod -R 777 /home/mathieu/Dropbox/Camera\ Uploads
 /usr/bin/python /home/mathieu/Git/sortphotos/src/sortphotos.py --silent /home/mathieu/Dropbox/Camera\ Uploads /home/mathieu/Dropbox/Photos --sort %Y-%m-%d --rename %Y_%m%d_%H%M > /dev/null 2>&1
