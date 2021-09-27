00bc5305b42828c44a25bd653510fefd23fc231c
mathieu <mcornille@gmail.com>
2021-09-27 21:40:47 +0100

    Adding Jane's mobile on Dropbox

--- bash/run_sortphotos.sh
+++ bash/run_sortphotos.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#Mathieu/Ruth Camera Uploads
 /bin/chmod -R 777 /home/mathieu/Dropbox/Camera\ Uploads
 /usr/bin/python /home/mathieu/Git/sortphotos/src/sortphotos.py --silent /home/mathieu/Dropbox/Camera\ Uploads /home/mathieu/Dropbox/Photos --sort %Y-%m-%d --rename %Y_%m%d_%H%M > /dev/null 2>&1
+/bin/chmod -R 777 /home/mathieu/Dropbox/Photos
+
+#Jane Camera Uploads
+/bin/chmod -R 777 /home/mathieu/Dropbox/Camera\ Uploads\ \(1\)
+/usr/bin/python /home/mathieu/Git/sortphotos/src/sortphotos.py --silent /home/mathieu/Dropbox/Camera\ Uploads\ \(1\) /home/mathieu/Dropbox/Photos --sort %Y-%m-%d --rename %Y_%m%d_%H%M > /dev/null 2>&1
 /bin/chmod -R 777 /home/mathieu/Dropbox/Photos
